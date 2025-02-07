55299147c9e094dcf7c94d994b613a9f1f384d2e
Walter Schmidt Marinho <walter.marinho@grupovoalle.com.br>
2025-02-07 17:51:56 -0300

    Database connections and migrations

--- CatalogoAPI/Models/Category.cs
+++ CatalogoAPI/Models/Category.cs
@@ -1,8 +1,17 @@
+using System.Collections.ObjectModel;
+
 namespace CatalogoAPI.Models;
 
 public class Category
 {
+    public Category()
+    {
+        Products = new Collection<Product>();
+    }
+
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? ImageUrl { get; set; }
+
+    public ICollection<Product>? Products { get; set; }
 }
